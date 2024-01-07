cd07837b05d441417402c7e9da67db58576fa0e6
Deathstar606 <dsfardin606@gmail.com>
2024-01-07 12:35:53 +0600

    Back-Comm

---
+++ app.js
@@ -0,0 +1,103 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var session = require('express-session');
+//var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+//var authenticate = require('./authenticate');
+var bodyParser = require('body-parser');
+var config = require('./config');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var leaderRouter = require('./routes/leaderRouter');
+var promoRouter = require('./routes/promoRouter');
+var favRouter = require('./routes/favouriteRouter');
+var commentRouter = require('./routes/commentRouter')
+var uploadRouter = require('./routes/uploadRouter');
+
+const mongoose = require('mongoose');
+
+mongoose.set('useCreateIndex', true)
+
+// Connection URL
+const url = config.mongoUrl
+const connect = mongoose.connect(url, 
+  { useNewUrlParser: true, 
+    useUnifiedTopology: true })
+ 
+connect.then((db) => {
+    console.log("Connection OK!");
+}, (err) => { console.log(err); });
+
+
+var app = express();
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + 3443 + req.url);
+  }
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(passport.initialize());
+
+app.use('/', index);
+app.use('/users', users);
+
+app.options('/users/login', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.status(204).send(); // No content needed for preflight response
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // Replace with your frontend's URL
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/dishes', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
+app.use('/favorites', favRouter);
+app.use('/comments', commentRouter)
+app.use('/imageUpload',uploadRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/promotions.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var promoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
+});
+var Promotions = mongoose.model('Promotion', promoSchema);
+
+module.exports = Promotions;
